42ce8e0a95d125afa497cc88363042abf206da27
zozeyx <126737048+zozeyx@users.noreply.github.com>
2024-05-07 16:53:22 +0900

    Update sed1.cpp

--- sed1.cpp
+++ sed1.cpp
@@ -1,84 +1,64 @@
-#include "Common.h"
+#include "../Common.h"
 
-#define SERVER_IP "127.0.0.1"
+char *SERVERIP = (char *)"127.0.0.1";
 #define SERVERPORT 9000
-#define BUFSIZE 512
-#define TOTAL_PACKETS 6 // 전체 패킷 개수
+#define BUFSIZE    512
+#define PACKET_COUNT 6
 
-typedef struct {
-    int seq_num;
-    char message[BUFSIZE];
-} Packet;
+// 패킷을 전송하는 함수
+void send_packet(int seq_num) {
+    // 패킷을 전송하는 코드 추가
+    char packet_msg[BUFSIZE];
+    sprintf(packet_msg, "packet %d", seq_num);
+    send(sock, packet_msg, strlen(packet_msg), 0);
+    printf("\"packet %d\" is transmitted.\n", seq_num);
+}
 
-int main(int argc, char *argv[]) {
+// Timeout 이벤트를 처리하는 함수
+void timeout_event(int seq_num) {
+    // Timeout 이벤트를 처리하는 코드 추가
+    printf("\"packet %d\" is timeout.\n", seq_num);
+    send_packet(seq_num);
+}
+
+int main(int argc, char *argv[])
+{
     int retval;
+
+    // 명령행 인수가 있으면 IP 주소로 사용
+    if (argc > 1) SERVERIP = argv[1];
 
     // 소켓 생성
     SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock == INVALID_SOCKET) err_quit("socket()");
 
-    // 서버 정보 설정
+    // connect()
     struct sockaddr_in serveraddr;
     memset(&serveraddr, 0, sizeof(serveraddr));
     serveraddr.sin_family = AF_INET;
-    serveraddr.sin_addr.s_addr = inet_addr(SERVER_IP);
+    inet_pton(AF_INET, SERVERIP, &serveraddr.sin_addr);
     serveraddr.sin_port = htons(SERVERPORT);
-
-    // 서버에 연결
     retval = connect(sock, (struct sockaddr *)&serveraddr, sizeof(serveraddr));
     if (retval == SOCKET_ERROR) err_quit("connect()");
 
-    Packet packets[TOTAL_PACKETS];
-    int next_seq_num = 0;
-    int acked[TOTAL_PACKETS] = {0}; // 각 패킷의 ACK 상태를 추적
+    // 데이터 통신에 사용할 변수
+    char buf[BUFSIZE + 1];
+    int len;
 
-    // 처음 4개 패킷 생성
-    for (int i = 0; i < 4 && i < TOTAL_PACKETS; i++) {
-        packets[i].seq_num = i;
-        sprintf(packets[i].message, "Data for packet %d", i);
-    }
+    // 서버와 데이터 통신
+    int packets_sent = 0;
+    while (packets_sent < PACKET_COUNT) {
+        // 패킷 전송
+        send_packet(packets_sent);
+        packets_sent++;
+        usleep(1); // 1 time unit 대기
 
-    // 처음 4개 패킷 전송
-    for (int i = 0; i < 4 && i < TOTAL_PACKETS; i++) {
-        retval = send(sock, (char *)&packets[i], sizeof(Packet), 0);
-        if (retval == SOCKET_ERROR) {
-            err_display("send()");
-            break;
-        }
-        printf("* \"packet %d\" is transmitted.\n", packets[i].seq_num);
-    }
-
-    // 이후 패킷 전송
-    while (next_seq_num < TOTAL_PACKETS) {
-        // ACK 기다리기
-        int ack;
-        retval = recv(sock, (char *)&ack, sizeof(int), 0);
-        if (retval == SOCKET_ERROR) {
-            err_display("recv()");
-            break;
-        } else if (retval == 0) {
-            printf("Server disconnected.\n");
-            break;
-        }
-
-        // 올바른 ACK인지 확인
-        if (ack >= 0 && ack < TOTAL_PACKETS) {
-            printf("* \"ACK %d\" is received.\n", ack);
-            acked[ack] = 1; // 해당 ACK 처리 완료
-            // 다음 패킷 전송
-            while (next_seq_num < TOTAL_PACKETS && acked[next_seq_num]) {
-                next_seq_num++;
-            }
-            if (next_seq_num < TOTAL_PACKETS) {
-                retval = send(sock, (char *)&packets[next_seq_num], sizeof(Packet), 0);
-                if (retval == SOCKET_ERROR) {
-                    err_display("send()");
-                    break;
-                }
-                printf("* \"packet %d\" is transmitted.\n", packets[next_seq_num].seq_num);
-            }
-        } else {
-            printf("* Unexpected ACK received: %d.\n", ack);
+        // Timeout 이벤트 처리
+        static int timer = 0;
+        timer++;
+        if (timer == TIMEOUT_INTERVAL) {
+            timeout_event(packets_sent - 1);
+            timer = 0;
         }
     }
 
