bb84305e83e2ad29094b487b368ae929bf43f58c
zozeyx <126737048+zozeyx@users.noreply.github.com>
2024-05-07 16:53:04 +0900

    Update rec1.cpp

--- rec1.cpp
+++ rec1.cpp
@@ -1,17 +1,30 @@
-#include "Common.h"
+#include "../Common.h"
 
 #define SERVERPORT 9000
-#define BUFSIZE 512
-#define WINDOW_SIZE 4
-#define TIMEOUT_INTERVAL 5
-#define TOTAL_PACKETS 6 // 전체 패킷 개수
+#define BUFSIZE    512
+#define PACKET_COUNT 6
 
-typedef struct {
-    int seq_num;
-    char message[BUFSIZE];
-} Packet;
+void receive_packet(int seq_num) {
+    // 패킷을 수신하는 함수
+    printf("\"packet %d\" is received.\n", seq_num);
+}
 
-int main(int argc, char *argv[]) {
+void send_ack(int ack_num, SOCKET client_sock, struct sockaddr_in clientaddr) {
+    // ACK를 송신하는 함수
+    char ack_msg[BUFSIZE];
+    sprintf(ack_msg, "ACK %d", ack_num);
+    send(client_sock, ack_msg, strlen(ack_msg), 0);
+    printf("\"ACK %d\" is transmitted.\n", ack_num);
+}
+
+void timeout_event(int seq_num) {
+    // Timeout 이벤트를 처리하는 함수
+    printf("\"packet %d\" is timeout.\n", seq_num);
+    send_packet(seq_num);
+}
+
+int main(int argc, char *argv[])
+{
     int retval;
 
     // 소켓 생성
@@ -35,7 +48,7 @@ int main(int argc, char *argv[]) {
     SOCKET client_sock;
     struct sockaddr_in clientaddr;
     socklen_t addrlen;
-    Packet packet;
+    char buf[BUFSIZE + 1];
 
     while (1) {
         // accept()
@@ -46,33 +59,44 @@ int main(int argc, char *argv[]) {
             break;
         }
 
+        // 접속한 클라이언트 정보 출력
+        char addr[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &clientaddr.sin_addr, addr, sizeof(addr));
+        printf("\n[TCP 서버] 클라이언트 접속: IP 주소=%s, 포트 번호=%d\n",
+               addr, ntohs(clientaddr.sin_port));
+
         // 클라이언트와 데이터 통신
+        int expected_seq = 0;
         while (1) {
             // 데이터 받기
-            retval = recv(client_sock, (char *)&packet, sizeof(Packet), 0);
+            retval = recv(client_sock, buf, BUFSIZE, 0);
             if (retval == SOCKET_ERROR) {
                 err_display("recv()");
                 break;
             } else if (retval == 0)
                 break;
 
-            printf("* \"packet %d\" is received.\n", packet.seq_num);
+            // 받은 데이터 출력
+            buf[retval] = '\0';
+            receive_packet(expected_seq);
+            expected_seq = (expected_seq + 1) % PACKET_COUNT;
 
             // ACK 보내기
-            retval = send(client_sock, (char *)&packet.seq_num, sizeof(int), 0);
-            if (retval == SOCKET_ERROR) {
-                err_display("send()");
-                break;
-            }
-            printf("* \"ACK %d\" is transmitted.\n", packet.seq_num);
+            send_ack(expected_seq - 1, client_sock, clientaddr);
 
-            // 모든 패킷을 수신했을 경우 루프 탈출
-            if (packet.seq_num == TOTAL_PACKETS - 1)
-                break;
+            // Timeout 이벤트 처리
+            static int timer = 0;
+            timer++;
+            if (timer == TIMEOUT_INTERVAL) {
+                timeout_event(expected_seq - 1);
+                timer = 0;
+            }
         }
 
         // 소켓 닫기
         close(client_sock);
+        printf("[TCP 서버] 클라이언트 종료: IP 주소=%s, 포트 번호=%d\n",
+               addr, ntohs(clientaddr.sin_port));
     }
 
     // 소켓 닫기
